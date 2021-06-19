bcc64d4e0865ddea78b8b6ffea0b792461e35ab3
jbped <pedigojacob@gmail.com>
2021-06-19 16:56:02 -0600

    updated link to contributor covenant

--- src/readme-template.js
+++ src/readme-template.js
@@ -167,7 +167,7 @@ let newTemplateLiteral =
 // Selects the appropriate response for the Contribution section. IF confirmContributions === false, provide  Contributor Covenant.
 const contributionLogic = (confirmContributions, contributions) => {
     if (!confirmContributions) {
-        return `Contributions to this project follow the Contributor Covenant [additional information can be found here](https://www.contributor-covenant.org/)`
+        return `Contributions to this project follow the Contributor Covenant [additional information can be found here](https://www.contributor-covenant.org/version/2/0/code_of_conduct/).`
     } else {
         return `${contributions}`
     }
